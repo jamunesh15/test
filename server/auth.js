9867ec699e1320f2d06ecace3a57b46f54335d0d
krishnalitaliya07-ops <jsheta15@gmail.com>
2026-04-25 02:57:13 +0530

    auth 2

--- server/auth.js
+++ server/auth.js
@@ -2,3 +2,7 @@
 console.log('====================================');
 console.log("UTH BRANCH ");
 console.log('====================================');
+
+console.log('====================================');
+console.log("takign merging conflict ");
+console.log('====================================');
